0854560aa584c7e8652377a2549b77adcd3b7742
Jayesh Kumar <jayeshiiitb@gmail.com>
2026-07-02 17:01:49 +0530

    fixed all cast case sensitive issues

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,5 +1,5 @@
 const Order = require("../models/Order");
-const Cart = require("../models/Cart");
+const Cart = require("../models/cart");
 const User = require("../models/User");
 const sendEmail = require("../utils/sendEmail");
 const Product = require("../models/Product");
